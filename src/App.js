2eb9c22cd16fc97df84aa62f7b7da09424566efd
Bekzat247 <toktobekzat@gmail.com>
2023-08-31 18:31:46 +0600

    fix: vars

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import CreateTodo from './components/CreateTodo/CreateTodo'
 import TodoContainer from './components/TodoContainer/TodoContainer';
 import { useState } from 'react';
 import Filter from './components/filter/filter';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 
 function App() {
   const [status, setStatus] = useState('All') 
-  const todosArr = useSelector((state) => state.todos)
+  // const todosArr = useSelector((state) => state.todos)
 
-
-  const newTodo = todosArr.filter((item) => {
-    if (status ==='All') return item
-    if (status === 'compleate' && item.status) return item
-    if (status === "Progress" && !item.status) return item
-  })
+  //This is Filter! Thats be used in future!
+  // const newTodo = todosArr.filter((item) => {
+  //   if (status ==='All') return item
+  //   if (status === 'compleate' && item.status) return item
+  //   if (status === "Progress" && !item.status) return item
+  // })
 
 
   return (
